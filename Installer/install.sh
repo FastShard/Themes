9cc8f4ed7163921f90c179ebb0143ad9adb5711c
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-26 21:14:25 +0100

    Create install.sh

---
+++ Installer/install.sh
@@ -0,0 +1,16 @@
+# Create the Themes Directory
+echo "🢒 Creating Directories"
+mkdir ~/.config/fastshard > /dev/null
+
+# Install Themes
+echo "🢒 Installing Themes"
+cd ~/ && git clone https://github.com/FastShard/Themes && cd ~/Themes && cp -d * ~/.config/fastshard > /dev/null
+rm -rf ~/Themes > /dev/null
+
+# Install Binary
+echo "🢒 Installing Binary"
+curl -s https://raw.githubusercontent.com/FastShard/Binary/main/Binary/fastshard -o fastshard > /dev/null
+sudo chmod +x fastshard > /dev/null
+mv fastshard /usr/bin/ > /dev/null
+echo " "
+echo "🢒 ThemeSwitcher has been installed. Run it using 'fastshard <theme>' in your Terminal"
