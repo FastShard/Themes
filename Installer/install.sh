12c2b78f68ab3026c56e915b7be4297a49c2ce64
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-30 23:04:32 +0100

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -13,7 +13,7 @@ mkdir ~/.config/fastshard > /dev/null
 echo "🢒 Installing Themes"
 cd ~/ && git clone -q https://github.com/FastShard/Themes > /dev/null && cd ~/Themes && cp -r * ~/.config/fastshard > /dev/null
 rm -rf ~/Themes > /dev/null
-sudo cp ~/.config/fastshard/lotus/bspwm/bin/themechanger.sh ~/.config/bspwm/bin/
+#sudo cp ~/.config/fastshard/lotus/bspwm/bin/themechanger.sh ~/.config/bspwm/bin/
 #sudo cp ~/.config/fastshard/lotus/sxhkd/sxhkdrc ~/.config/sxhkd/
 bspc wm -r
 
