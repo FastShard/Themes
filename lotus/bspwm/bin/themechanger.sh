ddad81855efb6fc48a0129099f6a71c0e24aeb8f
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-30 08:09:03 +0100

    Update themechanger.sh

--- lotus/bspwm/bin/themechanger.sh
+++ lotus/bspwm/bin/themechanger.sh
@@ -9,14 +9,14 @@ echo "$selected"
 if [ "$selected" = "Hord" ]
 then
 	fastshard hord
-	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/hord/custom.css -o /.config/BetterDiscord/data/stable/custom.css
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/hord/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
 	bspc wm -r
 	exit
 fi
 if [ "$selected" = "Lotus" ]
 then
-        fastshard lotus
-	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/lotus/custom.css -o /.config/BetterDiscord/data/stable/custom.css
+    fastshard lotus
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/lotus/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
 	bspc wm -r
 	exit
 fi
