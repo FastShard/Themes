7d67c0a1e469d112b89049afc4d43c828380846b
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-30 18:00:50 +0100

    Create themechanger.sh

---
+++ catppuccin/bspwm/bin/themechanger.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+selected=$(echo "Hord
+Lotus
+Catppuccin" | rofi -dmenu -theme ~/.config/bspwm/rofi/themes/launcher.rasi)
+
+echo "$selected"
+
+if [ "$selected" = "Hord" ]
+then
+	fastshard hord
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/hord/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
+	bspc wm -r
+	exit
+fi
+if [ "$selected" = "Lotus" ]
+then
+    fastshard lotus
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/lotus/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
+	bspc wm -r
+	exit
+fi
