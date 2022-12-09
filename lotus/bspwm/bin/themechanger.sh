59e8365db6f5d123647e99f27a6d139a65d51f63
Gast <117211251+0xGast@users.noreply.github.com>
2022-12-09 13:26:11 +0100

    Update themechanger.sh

--- lotus/bspwm/bin/themechanger.sh
+++ lotus/bspwm/bin/themechanger.sh
@@ -3,8 +3,7 @@
 selected=$(echo "Hord
 Lotus
 Catppuccin
-Mountain
-Minimal" | rofi -dmenu -theme ~/.config/bspwm/rofi/themes/launcher.rasi)
+Mountain" | rofi -dmenu -theme ~/.config/bspwm/rofi/themes/launcher.rasi)
 
 echo "$selected"
 
@@ -33,13 +32,6 @@ if [ "$selected" = "Mountain" ]
 then
    	fastshard mountain
 	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/mountain/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
-	bspc wm -r
-	exit
-fi
-if [ "$selected" = "Minimal" ]
-then
-   	fastshard minimal
-	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/minimal/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
 	bspc wm -r
 	exit
 fi
